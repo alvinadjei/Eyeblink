f942c48eb7d67d706c24eb5c8b61b16381b4de38
alvinadjei <alvinadjei@college.harvard.edu>
2024-12-09 14:32:24 -0800

    Arduino code for ir leds

---
+++ Arduino/ir_led/ir_led.ino
@@ -0,0 +1,18 @@
+int ledPin1 = 8;
+int ledPin2 = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledPin1, HIGH);
+  digitalWrite(ledPin2, LOW);
+  delay(500);  // Duration of the air puff (250 ms)
+  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin2, HIGH);
+  delay(500);  // Rest between air puffs (250 ms)
+}
